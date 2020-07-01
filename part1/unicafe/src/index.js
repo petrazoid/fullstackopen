905b6e5a88407d655a45fd4c3bce153d43d8edfb
Petra <petra.stumpf@gmail.com>
2020-07-01 12:56:28 -0700

    part1.7

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -6,29 +6,36 @@ const App = () => {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
+  const [allClicks, setAll] = useState([0]);
 
   const handleGoodClick = () => {
     setGood(good + 1);
+    setAll(allClicks.concat(1));
   };
 
   const handleNeutralClick = () => {
     setNeutral(neutral + 1);
+    setAll(allClicks.concat(0));
   };
 
   const handleBadClick = () => {
     setBad(bad + 1);
+    setAll(allClicks.concat(-1));
   };
 
   return (
     <div>
       <h1>give feedback</h1>
       <button onClick={handleGoodClick}>good</button>
-      {good}
       <button onClick={handleNeutralClick}>neutral</button>
-      {neutral}
       <button onClick={handleBadClick}>bad</button>
-      {bad}
       <h1>statistics</h1>
+      <p>good {good}</p>
+      <p>neutral {neutral}</p>
+      <p>bad {bad}</p>
+      <p>all {allClicks}</p>
+      <p>average {allClicks.reduce((x, y) => x + y) / allClicks.length}</p>
+      <p>positive {good / allClicks.length}</p>
     </div>
   );
 };
